3316e65b2b2293df59a310e4b4ffcebf65196102
Gogh-office <4535292@qq.com>
2018-01-08 16:00:32 +0800

    learn vue-x

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,152 @@
+<template>
+  <div>
+    <button @click="add1">{{ nub1 | addYrs }}</button>
+    <button @click="add2">{{nub2}}</button>
+    <button @click="add3">{{nub1}}</button>
+    <button @click="add4">{{nub4}}</button>
+    <button @click="add5">{{nub4}}</button>
+    <div v-for="(item,index) in statusT" @click="changeS" :data-ind="index">{{item.text}}</div>
+    <div>Length: {{ statesTLen }}</div>
+    <div>2:{{getById(2)}}</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+// 引入 vuex 中的 mapState 方法
+
+import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  // 原始写法
+  //computed: {
+  //  nub1 (){
+  //    return this.$store.state.nub1
+  //  },
+  //  nub2 (){
+  //    return this.$store.state.nub2
+  //  }
+  //},
+
+  computed: {
+    ...mapGetters([
+      'getById'
+    ]),
+    ...mapState({
+    // 原始写法
+    //nub1: function(state){
+    //  return state.nub1
+    //},
+
+    // 变形 ES6
+    //nub1: state =>　state.nub1,
+
+    // 简写
+    nub1: 'nub1',
+    nub2: 'nub2',
+    nub4: 'nub4',
+  //  statusT: function(state){
+  //    return this.$store.getters.doneTodos
+  //  },
+  //  statesTLen: function(state, getters){
+  //    return this.$store.getters.doneTodos.length
+  //  }
+    statusT (state) {
+        return this.$store.getters.doneTodos
+    },
+    statesTLen (state, getters){
+      return this.$store.getters.doneTodos.length
+    },
+
+    // getters 传参 筛选
+    //getId: function(state){
+    //  return function(id){
+    //    return this.$store.getters.getById(id)
+    //  }
+    //},
+    getId: function(state){
+      return function(id){
+        return this.$store.getters.getById(id)
+      }
+    },
+
+    // 简写成 箭头函数 this 指向发生错误
+    //getId: (state) => (id) => {
+    // return this.$store.getters.getById(id)
+    //}
+
+
+    })
+  },
+
+  // 当映射的计算属性的名称与 state 的子节点名称相同时, 直接传入 数组
+  //computed: mapState([
+  //  'nub1', 'nub2'
+  //]),
+
+
+  methods: {
+    add1 (){
+      this.$store.commit('nubAdd')
+      var _this = this.$parent
+      var __this = _this.$parent
+      if(__this){
+        console.log(__this)
+      }
+    },
+    add2 (){
+      this.$store.commit('nubAdd2')
+    },
+    add3 (){
+      this.$store.commit('nubAdd3')
+    },
+    add4 (){
+      // 原始方法 直接传参
+      // this.$store.commit('nubAdd4', 10)
+
+      // 变形  传一个对象  实参是 payload.amount
+      // this.$store.commit('nubAdd4', { amount: 33 })
+
+      // 变形 通用对象
+      this.$store.commit({
+        type: 'nubAdd4',
+        amountSS: 10,
+        XX: 2
+      })
+    },
+    add5 (){
+      this.$store.commit('SOME_MUTATIONcc')
+    },
+    changeS (e){
+    let Ind = e.target.getAttribute('data-ind')
+      this.$store.commit('changeS',Ind)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,109 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+
+// 引入常量替代 Mutation 事件
+import { SOME_MUTATION } from './mutation-types'
+Vue.config.productionTip = false
+Vue.use(Vuex)
+// 全局过滤器
+Vue.filter('addYrs', function(val){
+  return val + ' 年'
+})
+const store = new Vuex.Store({
+  state: {
+    nub1: 2018,
+    nub2: 2000,
+    nub4: 1,
+    storeIn: this.MainData,
+    todos: [
+      {
+        id: 1,
+        text: '1text',
+        done: true
+      },
+      {
+        id: 2,
+        test: '2text',
+        done: false
+      },
+      {
+        id: 3,
+        text: '3text',
+        done: true
+      },
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      //return state.todos.filter( todo => todo.done )
+
+      return state.todos.filter(function(todo){
+        return todo.done
+      })
+    },
+    //getById: (state) => (id) => {
+    //  return state.todos.find(todo => todo.id === id)
+    //},
+    getById: function(state){
+      return function(id){
+        // return state.todos.find(todo => todo.id === id)
+        return state.todos.find(function(todo){
+          return todo.id === id
+        })
+      }
+    },
+    changeSS: function(){
+      return 'SSS'
+    }
+  },
+  methods: {
+    setKey: function(){
+      this.$set(App2018.MainData, 'c', '通过子组件的事件添加')
+    }
+  },
+  mutations: {
+    nubAdd: function(state){
+      state.nub1 ++
+
+    },
+    // 动态 mutations 方法  真正方法名 在 mutation-types.js 中定义 对应到 子组件促发的事件
+    [SOME_MUTATION] (state){
+      state.nub4--
+    },
+    nubAdd2 (state){
+      state.nub2 +=2
+    },
+    nubAdd3 (state){
+      state.nub1 +=5
+    },
+    nubAdd4 (state, payload){
+      state.nub4 += payload.amountSS
+      state.nub4 *= payload.XX
+    },
+    changeS (state,ind){
+      state.todos[ind].done = false
+    }
+  }
+})
+/* eslint-disable no-new */
+const App2018 = new Vue({
+  el: '#app',
+  store,
+  router,
+  data (){
+    return {
+      MainData: {
+        a: 'aa'
+      }
+    }
+  },
+  template: '<App/>',
+  components: { App }
+})
+console.log(App2018)
+//  不能直接在 data 中添加对象  只能 在已存在对象上 添加 键
+Vue.set(App2018.MainData, 'b', '新添加的键 b')
